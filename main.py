06ea532d2db8d531145e8b20aa953d014392db7b
gabrielchiaradia <gabriel.chiaradia@gmail.com>
2026-03-22 11:58:29 -0300

    Eliminado predio de FilterContext

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def main():
     logger.info("="*50)
     client = get_client()
     balance = get_futures_balance(client)
-    balanceinicial = round(balance, 0)
+    balanceinicial = int(balance)
     print(balanceinicial)
     alert_startup(SYMBOL, RISK_PER_TRADE, TP_RR_RATIO )
     set_leverage(client, SYMBOL)
